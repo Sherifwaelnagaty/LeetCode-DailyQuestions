6bd24038d1b7b0abc3370caf4ae286a81a480152
Sherif Wael <85075288+Sherifwaelnagaty@users.noreply.github.com>
2024-07-13 05:28:11 +0300

    [215] [Time Beats: 94.94%] [Memory Beats: 17.20%] - LeetPush

--- 215-Kth-Largest-Element-in-an-Array.py
+++ 215-Kth-Largest-Element-in-an-Array.py
@@ -1,5 +1,25 @@
 class Solution:
-    def findKthLargest(self, nums, k):
-        return sorted(nums, reverse=True)[k-1]
-             
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        def quick_select(nums, k):
+            small = []
+            large = []
+            equal = []
+
+            pivot_num = random.choice(nums)
+
+            for num in nums:
+                if num == pivot_num:
+                    equal.append(num)
+                elif num < pivot_num:
+                    small.append(num)
+                else:
+                    large.append(num)
             
+            if k <= len(large):
+                return quick_select(large, k)
+            
+            if k > len(large) + len(equal):
+                return quick_select(small, k - len(large) - len(equal))
+            
+            return pivot_num
+        return quick_select(nums, k)
